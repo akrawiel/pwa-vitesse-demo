d7490c90ad6c5d1e13928bcedb2756c2e36e7424
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2021-01-20 15:13:53 +0100

    first version of functional qr code scanning

---
+++ src/pages/index.setup.ts
@@ -0,0 +1,133 @@
+import { computed, ref, watch } from 'vue'
+import { get, set, templateRef, useRafFn, useWebWorkerFn } from '@vueuse/core'
+
+const userMediaConstraints = {
+  video: {
+    width: 320,
+    height: 320,
+    frameRate: 30,
+    resizeMode: 'crop-and-scale',
+    facingMode: 'environment',
+  },
+}
+
+const IndexSetup = () => {
+  const videoRef = templateRef<HTMLVideoElement>('video')
+  const canvasRef = templateRef<HTMLCanvasElement>('canvas')
+
+  const streamRef = ref<MediaStream | null>(null)
+  const qrCodeRef = ref<{ data: string } | null>(null)
+
+  const { workerFn: detectQR, workerStatus: detectQRStatus } = useWebWorkerFn(
+    (imageData, width, height) => {
+      // eslint-disable-next-line no-undef
+      return jsQR(imageData, width, height, {
+        inversionAttempts: 'attemptBoth',
+      })
+    },
+    {
+      dependencies: [
+        'https://unpkg.com/jsqr@1.3.1/dist/jsQR.js',
+      ],
+    },
+  )
+
+  const { pause: pauseScanning, resume: resumeScanning } = useRafFn(() => {
+    const ctx = get(canvasRef)?.getContext('2d')
+    const { height, width } = get(canvasRef) ?? {}
+
+    if (ctx) {
+      ctx.drawImage(
+        videoRef.value,
+        0,
+        0,
+        width,
+        height,
+      )
+
+      if (detectQRStatus.value !== 'RUNNING') {
+        const imageData = ctx.getImageData(0, 0, width, height)
+
+        detectQR(imageData.data, width, height).then(
+          (qrReturn) => {
+            set(qrCodeRef, qrReturn)
+          },
+        )
+      }
+    }
+  }, {
+    immediate: false,
+  })
+
+  const qrCodeData = computed(
+    () => get(qrCodeRef)?.data ?? null,
+  )
+
+  const isScanning = computed(() => Boolean(get(streamRef)))
+
+  const isQrCodeLink = computed(
+    () => get(qrCodeData)?.startsWith('http'),
+  )
+
+  const deactivateStream = () => {
+    if (videoRef.value) {
+      videoRef.value.srcObject = null
+      set(streamRef, null)
+    }
+  }
+
+  const toggleScanning = () => {
+    if (get(isScanning)) {
+      deactivateStream()
+    }
+    else {
+      navigator.mediaDevices.getUserMedia(userMediaConstraints).then(
+        (stream) => {
+          videoRef.value.srcObject = stream
+          set(streamRef, stream)
+        },
+      )
+    }
+  }
+
+  watch(
+    qrCodeData,
+    (qrCodeDataValue) => {
+      if (qrCodeDataValue)
+        deactivateStream()
+    },
+    {
+      immediate: true,
+    },
+  )
+
+  watch(
+    isScanning,
+    (isScanningValue) => {
+      if (isScanningValue) {
+        resumeScanning()
+      }
+      else {
+        pauseScanning()
+        get(canvasRef)?.getContext('2d')?.clearRect(
+          0,
+          0,
+          get(canvasRef)?.width,
+          get(canvasRef)?.height,
+        )
+      }
+    },
+    {
+      immediate: true,
+    },
+  )
+
+  return {
+    isScanning,
+    isQrCodeLink,
+    toggleScanning,
+    qrCodeData,
+  }
+}
+
+export default IndexSetup
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,10 +3,14 @@
     <div class="text-center">
       Scanning QR code
     </div>
-    <div class="flex flex-1 items-center">
-      <canvas id="canvas" ref="canvas" width="320" height="320" />
+    <div class="canvasContainer">
+      <canvas ref="canvas" width="320" height="320" />
+      <div v-if="!isScanning" class="canvasOverlay">
+        <div><icon-tap class="inline-block mr-2" />Press "Start scanning"</div>
+        <div><icon-camera class="inline-block mr-2" />Enable camera access</div>
+      </div>
     </div>
-    <video id="video" ref="video" autoplay muted />
+    <video ref="video" autoplay muted />
     <div class="mb-4">
       Scanned code:
     </div>
@@ -15,141 +19,48 @@
       :class="isQrCodeLink ? 'link' : ''"
       :href="qrCodeData"
     >
-      <b>{{ qrCodeData ?? 'N/A' }}</b>
+      <b>{{ qrCodeData ?? '&nbsp;' }}</b>
     </component>
-    <button class="btn mt-5" @click="startScanning">
+    <button class="btn mt-5" @click="toggleScanning">
       {{ isScanning ? 'Stop scanning' : 'Start scanning' }}
     </button>
   </div>
 </template>
 
 <script lang="ts">
-import { computed, onMounted, ref, watch } from 'vue'
-import { get, set, templateRef, useRafFn } from '@vueuse/core'
-import jsQR, { QRCode } from 'jsqr'
+import IconCamera from '/@vite-icons/mdi/camera.vue'
+import IconTap from '/@vite-icons/mdi/gesture-tap-button.vue'
 
-const userMediaConstraints = {
-  video: {
-    width: 320,
-    height: 320,
-    frameRate: 30,
-    resizeMode: 'crop-and-scale',
-    facingMode: 'environment',
-  },
-}
+import IndexSetup from './index.setup'
 
 export default {
-  setup() {
-    const camOut = templateRef<HTMLVideoElement>('video')
-    const canvasRef = templateRef<HTMLCanvasElement>('canvas')
-
-    const streamRef = ref<MediaStream | null>(null)
-    const qrCodeRef = ref<QRCode | null>(null)
-
-    const { pause, resume } = useRafFn(() => {
-      const ctx = get(canvasRef)?.getContext('2d')
-      const { height, width } = get(canvasRef) ?? {}
-
-      if (ctx) {
-        ctx.drawImage(
-          camOut.value,
-          0,
-          0,
-          width,
-          height,
-        )
-
-        const imageData = ctx.getImageData(0, 0, width, height)
-
-        const qrCode = jsQR(imageData.data, width, height, {
-          inversionAttempts: 'attemptBoth',
-        })
-
-        set(qrCodeRef, qrCode)
-      }
-    }, {
-      immediate: false,
-    })
-
-    const isScanning = computed(() => Boolean(get(streamRef)))
-
-    const deactivateStream = () => {
-      if (camOut.value) {
-        camOut.value.srcObject = null
-        set(streamRef, null)
-      }
-    }
-
-    const qrCodeData = computed(
-      () => get(qrCodeRef)?.data ?? null,
-    )
-
-    watch(
-      qrCodeData,
-      (qrCodeDataValue) => {
-        if (qrCodeDataValue)
-          deactivateStream()
-      },
-      {
-        immediate: true,
-      },
-    )
-
-    watch(
-      isScanning,
-      (isScanningValue) => {
-        if (isScanningValue) {
-          resume()
-        }
-        else {
-          pause()
-          canvasRef.value?.getContext('2d')?.clearRect(
-            0,
-            0,
-            canvasRef.value?.width,
-            canvasRef.value?.height,
-          )
-        }
-      },
-      {
-        immediate: true,
-      },
-    )
-
-    const startScanning = () => {
-      if (get(isScanning)) {
-        deactivateStream()
-      }
-      else {
-        navigator.mediaDevices.getUserMedia(userMediaConstraints).then(
-          (stream) => {
-            camOut.value.srcObject = stream
-            set(streamRef, stream)
-          },
-        )
-      }
-    }
-
-    const isQrCodeLink = computed(
-      () => get(qrCodeData)?.startsWith('http'),
-    )
-
-    return {
-      isScanning,
-      isQrCodeLink,
-      startScanning,
-      qrCodeData,
-    }
+  components: {
+    IconCamera,
+    IconTap,
   },
+
+  setup: IndexSetup,
 }
 </script>
 
 <style scoped>
-#video {
+video {
   display: none;
 }
 
-#canvas {
-  @apply w-full rounded border border-black;
+canvas {
+  @apply w-full rounded border border-black dark:border-white;
+}
+
+.canvasContainer {
+  @apply flex flex-1 items-center relative w-full;
+}
+
+.canvasOverlay {
+  @apply z-10 text-center absolute p-4 w-max border border-black dark:border-white rounded;
+
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
 }
 </style>
